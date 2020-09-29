cc04570163593b776384767c6c630bc2449e88e8
Vlad Pavlyuchenko <vlad.pavl@icloud.com>
2020-09-29 23:46:29 +0300

    add header adaptive

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-
+   void function () {
+      const burger = document.querySelector('.icon-menu');
+      burger.addEventListener('click', () => {
+          burger.classList.toggle('burger-active');
+      });
+   }();
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -112,7 +112,7 @@ function deploy() {
 }
 
 function startwatch() {
-	watch(baseDir  + '/' + preprocessor + '/**/*', {usePolling: true}, styles);
+	watch(baseDir  + '/' + preprocessor + '/**/*', {usePolling: true, delay: 500}, styles);
 	watch(baseDir  + '/images/src/**/*.{' + imageswatch + '}', {usePolling: true}, images);
 	watch(baseDir  + '/**/*.{' + fileswatch + '}', {usePolling: true}).on('change', browserSync.reload);
 	watch([baseDir + '/js/**/*.js', '!' + paths.scripts.dest + '/*.min.js'], {usePolling: true}, scripts);
